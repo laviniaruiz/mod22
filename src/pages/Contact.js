2f7c31450a35bdef3660323cae09b524c03bb7d4
laviniaruiz <lavh_90@yahoo.com>
2023-03-05 22:28:04 -0500

    netlify added

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -14,7 +14,7 @@ export default function Contact() {
       </p>
 
       <p className="c2">
-        <Link to="https://github.com/laviniaruiz/mod2project">@Netlify</Link>
+        <Link to="https://app.netlify.com/sites/guileless-mousse-8d2d17/deploys/64025bc090f955000858b102">@Netlify</Link>
       </p>
 
         {/* <p className="c3">
